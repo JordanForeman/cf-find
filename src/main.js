ce5171ad957ecb24577bb34f29dac6850dba64cf
Jordan Foreman <me@jordanforeman.com>
2019-11-12 21:06:14 -0600

    fix: extra log from testing

--- src/main.js
+++ src/main.js
@@ -3,8 +3,6 @@ const { log } = require('./log');
 const { findDistribution } = require('./find-distribution');
 
 function main() {
-  console.log(JSON.stringify(getAlias));
-
   const alias = getAlias();
   log(`searching for CloudFront distribution with alias: ${alias}`);
   findDistribution(alias);
